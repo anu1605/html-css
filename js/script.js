cbe1ea24b692f1f5ad8c73ae2bbff3b07f3230f5
Anushka Tyagi <anushka.tyagi161998@gmail.com>
2023-02-27 12:51:11 +0530

    2

--- js/script.js
+++ js/script.js
@@ -1,13 +1,19 @@
 var clicked =false;
+var date = new Date();
 document.addEventListener('click',function(){
     if(!clicked){
+        var i = 0;
         clicked = true;
-        for(i = 0;i< 180;i++){
-            document.getElementsByClassName('timer')[0].style.transform = 'rotateZ(0deg + i)'
-        }
+        document.getElementsByClassName('timer')[0].style.animationName = 'rotateTimer';
+        document.getElementsByClassName('timer')[0].style.animationDuration = '60s';
+        document.getElementsByClassName('timer')[0].style.animationTimingFunction = 'linear';
 
     }
-    else clicked = false;
+    else {console.timeEnd();
+        clicked = false;
+        document.getElementsByClassName('timer')[0].style.animationName = 'none';
+        document.getElementById('display').innerHTML = 0;
+    }
 })
 
 
